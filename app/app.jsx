280c3828d9f55957518b5fa3b6f356f433fae370
Andrew Day <Andrew Day>
2017-04-26 17:02:30 +1000

    initial commit

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var ToDoApp = require('ToDoApp');
 //Load foundation-min
 require('style!css!foundation-sites/dist/foundation.min.css');
 
@@ -11,6 +12,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-    <p>boilerplate 3</p>,
+    <ToDoApp/>,
     document.getElementById('app')
 );
