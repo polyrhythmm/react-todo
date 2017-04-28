c1b1479a0ef710567cb3bff9aa27437c86f53276
Andrew Day <Andrew Day>
2017-04-28 15:05:01 +1000

    Added ToDoSearch testing

---
+++ app/tests/components/ToDoSearch.test.jsx
@@ -0,0 +1,40 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var ToDoSearch = require('ToDoSearch');
+describe('ToDoSearch', () => {
+  it('should exist', () => {
+    expect(ToDoSearch).toExist();
+  });
+
+  it('should call onSearch with entered input text', () => {
+
+    var searchText = 'Dog';
+    var spy = expect.createSpy();
+
+    var toDoSearch = TestUtils.renderIntoDocument(<ToDoSearch onSearch={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(toDoSearch));
+
+    toDoSearch.refs.searchText.value = searchText;
+
+    TestUtils.Simulate.change(toDoSearch.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, searchText);
+  });
+
+  it('should call onSearch with proper checked value', () => {
+    var spy = expect.createSpy();
+
+    var toDoSearch = TestUtils.renderIntoDocument(<ToDoSearch onSearch={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(toDoSearch));
+
+    toDoSearch.refs.showCompleted.checked = true;
+
+    TestUtils.Simulate.change(toDoSearch.refs.showCompleted);
+
+    expect(spy).toHaveBeenCalledWith(true, '');
+  });
+})
