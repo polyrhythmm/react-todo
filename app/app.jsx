498614ac290c9dfa67a13bf16de90992aaaef74a
Andrew Day <Andrew Day>
2017-05-01 16:30:24 +1000

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var ToDoApp = require('ToDoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+ store.subscribe(() => {
+   console.log('New state', store.getState());
+ })
+
+store.dispatch(actions.addToDo("Clean the yard"));
+store.dispatch(actions.setSearchText("yard"));
+store.dispatch(actions.toggleShowCompleted());
 //Load foundation-min
 require('style!css!foundation-sites/dist/foundation.min.css');
 
